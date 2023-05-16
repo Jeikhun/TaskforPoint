41ecddf47f4e8ce9eabebc94a343a1159d5f5329
Jeikhun Jalilov <jeykhunvj@code.edu.az>
2023-05-17 02:28:21 +0400

    created new task

---
+++ index.js
@@ -0,0 +1,9 @@
+const calculateBtn = document.getElementsByClassName("button");
+const calculateBmi = document.getElementsByClassName('calculateBmi');
+const informationModal = document.getElementsByClassName('information-modal');
+calculateBmi.addEventListener("click", ()=>{
+    informationModal.style.display="none";
+    console.log("a");
+
+
+}) 
